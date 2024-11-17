7e02eeb39e0ef10af4d10505910e9bdbf6941af5
AlexisObel <obelalexisadich@gmail.com>
2024-11-17 23:21:52 +0300

    dark mode

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1709645833';
+const cacheName = 'chirpy-1709646345';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
